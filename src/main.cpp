adf3d5d82479b1fe0b263643dd226ae7521dbe2a
Celeste <yuan.zx@outlook.com>
2022-01-03 18:26:34 +0800

    feat: improve clock counter

--- src/main.cpp
+++ src/main.cpp
@@ -15,29 +15,29 @@ constexpr auto height = 480;
 constexpr auto model = "../assets/228-1-NBSS.CNG.swc";
 
 void renderImage();
+void printClockInfo(std::chrono::nanoseconds nanoseconds) noexcept;
 
 int main()
 {
-    try {
-        auto start = std::chrono::system_clock::now();
+    try
+    {
+        auto start = std::chrono::steady_clock::now();
         renderImage();
-        auto stop = std::chrono::system_clock::now();
+        auto stop = std::chrono::steady_clock::now();
 
-        std::cout << "Render complete: \n";
-        std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::hours>(stop - start).count() << " hours\n";
-        std::cout << "          : " << std::chrono::duration_cast<std::chrono::minutes>(stop - start).count() << " minutes\n";
-        std::cout << "          : " << std::chrono::duration_cast<std::chrono::seconds>(stop - start).count() << " seconds\n";
-
+        printClockInfo(stop - start);
         cv::waitKey(0);
         return EXIT_SUCCESS;
     }
-    catch (std::exception& ex) {
+    catch (std::exception &ex)
+    {
         std::cerr << ex.what() << std::endl;
         return EXIT_FAILURE;
     }
 }
 
-void renderImage() {
+void renderImage()
+{
     NeuronScene scene;
     scene.load(model);
 
@@ -46,7 +46,27 @@ void renderImage() {
     Render render(width, height);
     render.draw(scene, camera);
 
-    auto& colorBuffer = render.get_frame_buffer();
+    auto &colorBuffer = render.get_frame_buffer();
     cv::Mat img(height, width, CV_8UC3, colorBuffer.data());
     cv::imshow(title, img);
+}
+
+void printClockInfo(std::chrono::nanoseconds nanoseconds) noexcept
+{
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(nanoseconds);
+    nanoseconds -= hours;
+
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(nanoseconds);
+    nanoseconds -= minutes;
+
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(nanoseconds);
+    nanoseconds -= seconds;
+
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(nanoseconds);
+
+    std::cout << "Render complete: " << std::endl
+              << "Time taken: " << std::setw(4) << hours.count() << " hours, " << std::endl
+              << "            " << std::setw(4) << minutes.count() << " minutes, " << std::endl
+              << "            " << std::setw(4) << seconds.count() << " seconds, " << std::endl
+              << "            " << std::setw(4) << milliseconds.count() << " milliseconds." << std::endl;
 }
